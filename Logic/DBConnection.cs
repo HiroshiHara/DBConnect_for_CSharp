1d88c6952217ac28cd910a80e5f9afa023e9770f
HiroshiHara <mrbob403@gmail.com>
2022-10-28 23:05:35 +0900

    save commit.

---
+++ Logic/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DBConnect.Logic
+{
+    internal static class DBConnection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["sqlsvr"].ConnectionString;
+        }
+
+        public static ExecQuery(string query)
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+
+        }
+    }
+}
