0a24b0ae3063589560329409845e0ce8f84171f5
HiroshiHara <mrbob403@gmail.com>
2022-10-29 10:15:30 +0900

    Implement DB connection sample by C#.

--- Logic/DBConnection.cs
+++ Logic/DBConnection.cs
@@ -41,5 +41,38 @@ namespace DBConnect.Logic
                 }
             }
         }
+
+        public static DataRowCollection ExecQuery(string query, Dictionary<string, string> param)
+        {
+            DataTable dataTable = new DataTable();
+            string connStr = GetConnectionString();
+            using (var conn = new SqlConnection(connStr))
+            using (var cmd = conn.CreateCommand())
+            {
+                try
+                {
+                    conn.Open();
+                    cmd.CommandText = query;
+                    if (param != null && param.Count > 0)
+                    {
+                        foreach (var p in param)
+                        {
+                            cmd.Parameters.Add(new SqlParameter(p.Key, p.Value));
+                        }
+                    }
+                    dataTable.Load(cmd.ExecuteReader());
+                    return dataTable.Rows;
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw e;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
     }
 }
---
+++ Main/Program.cs
@@ -0,0 +1,35 @@
+﻿using DBConnect.Logic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnect.Main
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string query1 = @"select * from dept;";
+            var result1 = DBConnection.ExecQuery(query1);
+            Console.WriteLine(result1.Count);
+            foreach (DataRow row in result1)
+            {
+                Console.WriteLine($"deptcd:{row.Field<string>("deptcd")}, deptnm:{row.Field<string>("deptnm")}");
+            }
+
+            string query2 = @"select * from emp where age >= @AGE";
+            var param = new Dictionary<string, string>();
+            param.Add("@AGE", "25");
+            var result2 = DBConnection.ExecQuery(query2, param);
+            Console.WriteLine(result2.Count);
+            foreach (DataRow row in result2)
+            {
+                Console.WriteLine($"empcd:{row.Field<string>("empcd")}, empnm:{row.Field<string>("empnm")}");
+            }
+
+        }
+    }
+}
